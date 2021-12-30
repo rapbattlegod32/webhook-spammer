3f6157b7f6dcfbe0abe3b0157d2525f30d34a422
god <70163067+rapbattlegod32@users.noreply.github.com>
2021-12-30 21:00:38 +0000

    changed most of the code
    I changed most of it because i realised i could be using a much more easier way

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import os
 import time
-import requests, random
+import requests
 from dhooks import Webhook
 import ctypes
 ctypes.windll.kernel32.SetConsoleTitleW("Webhook Spammer") # Sets console name.
 
 time.sleep(0.3)
 
-hook = input("[>>] Enter your webhook: ") # Prompts you to enter a webhook
+webhookinput = input("[>>] Enter your webhook: ") # Prompts you to enter a webhook
+
+discordwebhook = webhookinput
 
 time.sleep(0.1)
 print('[>>] What would you like to send to the webhook?')
 hookmessage = input('[>>] ') # Prompts you to enter a message e.g 'Hello', '@everyone', 'Nuked by youruser#0000'
-print('[>>] You have chose the message:', hookmessage)
-warning = input('[>>] Press enter to start')
+warning = input('[>>] Press enter to start') # When the enter key is pressed it starts the webhook
+
+data = {"content": hookmessage}
+response = requests.post

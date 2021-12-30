fc554e3ddcfa74691e67d241fc86c48eb3dd18b4
god <70163067+rapbattlegod32@users.noreply.github.com>
2021-12-30 20:45:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import os
-import requests, random
 import time
-from dhooks import Webhook 
+import requests, random
+from dhooks import Webhook
 import ctypes
-ctypes.windll.kernel32.SetConsoleTitleW("Webhook Spammer") # Sets console name
+ctypes.windll.kernel32.SetConsoleTitleW("Webhook Spammer") # Sets console name.
+
+time.sleep(0.3)
 
 hook = input("[>>] Enter your webhook: ") # Prompts you to enter a webhook
+
+time.sleep(0.1)
+print('[>>] What would you like to send to the webhook?')
+hookmessage = input('[>>] ') # Prompts you to enter a message e.g 'Hello', '@everyone', 'Nuked by youruser#0000'
+print('[>>] You have chose the message:', hookmessage)
+warning = input('[>>] Press enter to start')

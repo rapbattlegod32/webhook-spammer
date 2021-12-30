4e39fef0fd9ddfd2d26626f644a4db4eb3fd5da1
god <70163067+rapbattlegod32@users.noreply.github.com>
2021-12-30 18:44:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import os
 import requests, random
 from dhooks import Webhook 
 import ctypes
-ctypes.windll.kernel32.SetConsoleTitleW("monke#9362 group finder") # Sets console name
+ctypes.windll.kernel32.SetConsoleTitleW("Webhook Spammer") # Sets console name

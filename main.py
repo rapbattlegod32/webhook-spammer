db966653f70fae95b2d00f32fe75896dcde7db1b
god <70163067+rapbattlegod32@users.noreply.github.com>
2021-12-31 00:06:47 +0000

    added amount feature

--- main.py
+++ main.py
@@ -11,17 +11,22 @@ webhookinput = input("[>>] Enter your webhook: ") # Prompts you to enter a webho
 
 discordwebhook = webhookinput
 
-time.sleep(0.1)
+time.sleep(0.2)
 print('[>>] What would you like to send to the webhook?')
 hookmessage = input('[>>] ') # Prompts you to enter a message e.g 'Hello', '@everyone', 'Nuked by youruser#0000'
+time.sleep(0.2)
+print("[>>] How many times do you want it to send?")
+amountofmessages = input('[>>] ')
+
 
 start = time.time()
-data = {"content": hookmessage}
-response = requests.post(discordwebhook, json=data)
+for i in range(0,int(amountofmessages)):
+    data = {"content": hookmessage, "amount": amountofmessages}
+    response = requests.post(discordwebhook, json=data)
 
-print(response.status_code ,'Content has been recieved')
+print(response.status_code ,'| Content has been recieved')
 end = time.time()
 
-print(end - start) # Measures the time that it took.
+print('Completed in: ', end - start,"seconds") # Measures the time that it took.
 
 time.sleep(10000)
